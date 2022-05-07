9c88a1a376bfebfe42267d9569013a7c39b8f763
Drozd0f <600dan600@gmail.com>
2022-05-07 10:55:51 +0300

    DB Creating from ERD

---
+++ database/queries_creating_from_erd/copy_csv_tables.sql
@@ -0,0 +1,27 @@
+COPY users(user_id, email, password, first_name, last_name, middle_name, is_staff, country, city, address)
+FROM '/usr/src/csv_tables/users.csv'
+DELIMITER ',' CSV;
+
+COPY carts(cart_id, users_user_id, subtotal, total, cart_timestamp)
+FROM '/usr/src/csv_tables/carts.csv'
+DELIMITER ',' CSV;
+
+COPY categories(category_id, category_title, category_description)
+FROM '/usr/src/csv_tables/categories.csv'
+DELIMITER ',' CSV;
+
+COPY products(product_id, product_title, product_description, in_stock, price, slug, category_id)
+FROM '/usr/src/csv_tables/products.csv'
+DELIMITER ',' CSV;
+
+COPY cart_product(carts_cart_id, products_product_id)
+FROM '/usr/src/csv_tables/cart_products.csv'
+DELIMITER ',' CSV;
+
+COPY order_status(order_status_id, status_name)
+FROM '/usr/src/csv_tables/order_statuses.csv'
+DELIMITER ',' CSV;
+
+COPY orders(order_id, carts_cart_id, order_status_order_status_id, shipping_total, total, created_at, updated_at)
+FROM '/usr/src/csv_tables/orders.csv'
+DELIMITER ',' CSV;
